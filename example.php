11f705c968fa47bd761fca1f2d2a9914f0a95cb0
Moritz Walter <moritz.walter@the-systems.eu>
2020-08-15 22:31:22 +0200

    init

---
+++ example.php
@@ -0,0 +1,20 @@
+<a
+    href="https://www.puh.hosting/aff/[ID]"
+    target="_new">
+    <img style="border-style: none; width: 120px; height: 60px"
+         src="https://cdn.puh.hosting/puhhosting/Banner/gif/MusikBots.gif" alt="PuhHosting">
+</a>
+
+<a
+    href="https://www.puh.hosting/aff/[ID]"
+    target="_new">
+    <img style="border-style: none; width: 120px; height: 60px"
+         src="https://cdn.puh.hosting/puhhosting/Banner/gif/Querybot.gif" alt="PuhHosting">
+</a>
+
+<a
+    href="https://www.puh.hosting/aff/[ID]"
+    target="_new">
+    <img style="border-style: none; width: 120px; height: 60px"
+         src="https://cdn.puh.hosting/puhhosting/Banner/gif/Webspace.gif" alt="PuhHosting">
+</a>
